33b83f9ca27acf010c0f410d1de208fa3dab2282
Joe Taylor <joewtaylor55@gmail.com>
2026-01-22 08:52:38 +0000

    While loop - switch statements: adding in ConsoleMenu and main method structure

---
+++ src/main/java/ConsoleMenu.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ConsoleMenu {
+
+    private final Scanner scanner = new Scanner(System.in);
+
+    public void displayMenu() {
+        System.out.print("""
+                Welcome to the Recipe App!
+                1. View all recipes
+                2. Add a new recipe
+                3. Search recipes
+                0. Exit
+                Please select an option:
+                """);
+    }
+
+    public int getUserAction() {
+        while (!scanner.hasNextInt()) {
+            System.out.println("Invalid input. Please enter a number matching the action you wish to perform:");
+            scanner.next(); // Clear the invalid input
+        }
+        return scanner.nextInt();
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static void main(String[] args) {
+        ConsoleMenu menu = new ConsoleMenu();
+        boolean running = true;
+
+        while (running) {
+            menu.displayMenu();
+            int action = menu.getUserAction();
+
+            switch (action) {
+                case 1:
+                    System.out.println("View all recipes (not implemented)");
+                    break;
+                case 2:
+                    System.out.println("Add a new recipe (not implemented)");
+                    break;
+                case 3:
+                    System.out.println("Search recipes (not implemented)");
+                    break;
+                case 4:
+                    System.out.println("Exiting the application");
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Invalid action, please select a number from the list provided.");
+            }
+        }
+    }
 }
